cbe70cd7137a22c0a5b6b8ce2a22b6305f3ae59f
AlbandryAlQaseemi <82465813+AlbandryAlQaseemi@users.noreply.github.com>
2021-04-19 05:52:57 +0300

    Add files via upload

---
+++ checkNumber.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+class program
+{
+
+	static void checkNumber(string s)
+	{
+		int Leftsum = 0;
+		int Rightsum = 0;
+
+		for (int i = 0; i < s.Length / 2; i++)
+		{
+
+			Leftsum += (int)(Char.GetNumericValue(s[i]) -
+							Char.GetNumericValue('0'));
+			Rightsum += (int)(Char.GetNumericValue(s[s.Length -
+													1 - i]) -
+							Char.GetNumericValue('0'));
+		}
+
+		if (Leftsum == Rightsum)
+			Console.WriteLine("true");
+		else
+			Console.WriteLine("false");
+	}
+
+	static void Main()
+	{
+		string s = "78987";
+
+		checkNumber(s);
+	}
